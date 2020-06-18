401b5452d82fa21fcb106556cdd8aab63286fad2
iTrooz_ <team.mortelgames@gmail.com>
2020-06-18 18:21:55 +0200

    Bande d'idiots, la terre est héxagonale !!

--- src/fr/entasia/sanctions/commands/ban/BanCmd.java
+++ src/fr/entasia/sanctions/commands/ban/BanCmd.java
@@ -46,64 +46,105 @@ public class BanCmd extends Command {
 			return "§cSyntaxe : /"+cmd+" <pseudo> <temps/def> [raison]";
 		}else{
 			try{
-				ResultSet rs = Main.sql.fastSelectUnsafe("SELECT name,uuid,address FROM playerdata.global WHERE name=?", args[0]);
-				if(rs.next()) {
-					UUID uuid = UUID.fromString(rs.getString("uuid"));
-					User u = manager.getUser(uuid);
-					if (u == null) {
-						try {
-							u = manager.loadUser(uuid).get();
-						} catch (Exception e) {
-							e.printStackTrace();
-							return "§cImpossible de charger les données de cet utilisateur ! (Erreur LuckPerms, contacte iTrooz_)";
+				SanctionEntry se = null;
+				for(SanctionEntry lse : Utils.bans){
+					if(lse.on.equals(args[0])){
+						se = lse;
+						break;
+					}
+				}
+				if(se==null){
+					ResultSet rs = Main.sql.fastSelectUnsafe("SELECT name,uuid,address FROM playerdata.global WHERE name=?", args[0]);
+					if(rs.next()) {
+						UUID uuid = UUID.fromString(rs.getString("uuid"));
+						User u = manager.getUser(uuid);
+						if (u == null) {
+							try {
+								u = manager.loadUser(uuid).get();
+							} catch (Exception e) {
+								e.printStackTrace();
+								return "§cImpossible de charger les données de cet utilisateur ! (Erreur LuckPerms, contacte iTrooz_)";
+							}
+							if (u == null) return "§cImpossible de charger les données de cet utilisateur !";
 						}
-						if (u == null) return "§cImpossible de charger les données de cet utilisateur !";
+						if(u.hasPermission(banExcept).asBoolean()&&!sender.hasPermission("restricted.sancmaster"
+						)){
+							return "§cTu ne peut pas bannir ce joueur !";
+						}
 					}
-					if(u.hasPermission(banExcept).asBoolean()&&!sender.hasPermission("restricted.sancmaster"
-					)){
-						return "§cTu ne peut pas bannir ce joueur !";
+
+					se = new SanctionEntry();
+
+					if(args[1].equalsIgnoreCase("def")||args[1].equalsIgnoreCase("inf"))se.time = -1;
+					else{
+						se.time = TextUtils.timeToSeconds(args[1]);
+						if(se.time<=0)return "§cTemps "+args[1]+" invalide !";
 					}
-				}
 
-				SanctionEntry se = new SanctionEntry();
+					se.on = rs.getString("name");
+					se.ip = InetAddress.getByName(rs.getString("address")).getAddress();
+					se.by = sender.getName();
+					se.reason = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
+					if(se.reason.equals(""))se.reason = "Aucune";
+					se.when = Calendar.getInstance();
 
-				if(args[1].equalsIgnoreCase("def")||args[1].equalsIgnoreCase("inf"))se.time = -1;
-				else{
-					se.time = TextUtils.timeToSeconds(args[1]);
-					if(se.time<=0)return "§cTemps "+args[1]+" invalide !";
-				}
-
-				se.on = rs.getString("name");
-				se.ip = InetAddress.getByName(rs.getString("address")).getAddress();
-				se.by = sender.getName();
-				se.reason = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
-				if(se.reason.equals(""))se.reason = "Aucune";
-				se.when = Calendar.getInstance();
-
-				se.type = 0;
+					se.type = 0;
 
 
-				se.id = Utils.requ(1,"INSERT INTO history (`id`, `on`, `by`, `type`, `when`, `time`, `reason`) VALUES " +
-						"(?, ?, ?, ?, ?, ?, ?)", se.on, se.by, se.type, se.when.getTimeInMillis(), se.time, se.reason);
+					se.id = Utils.requ(1,"INSERT INTO history (`id`, `on`, `by`, `type`, `when`, `time`, `reason`) VALUES " +
+							"(?, ?, ?, ?, ?, ?, ?)", se.on, se.by, se.type, se.when.getTimeInMillis(), se.time, se.reason);
 
-				Main.sql.fastUpdate("INSERT INTO actuals (`id`, `on`, `by`, `type`, `when`, `time`, `reason`) VALUES " +
-						"(?, ?, ?, ?, ?, ?, ?)", se.id, se.on, se.by, se.type, se.when.getTimeInMillis(), se.time, se.reason);
+					Main.sql.fastUpdate("INSERT INTO actuals (`id`, `on`, `by`, `type`, `when`, `time`, `reason`) VALUES " +
+							"(?, ?, ?, ?, ?, ?, ?)", se.id, se.on, se.by, se.type, se.when.getTimeInMillis(), se.time, se.reason);
 
-				Utils.bans.add(se);
-				ProxiedPlayer p = Main.main.getProxy().getPlayer(rs.getString("name"));
-				if(p!=null)p.disconnect(Base.genBanReason(se, se.time == -1 ? "§8Indéfini" : TextUtils.secondsToTime(se.remaning())).create());
+					Utils.bans.add(se);
+					ProxiedPlayer p = Main.main.getProxy().getPlayer(rs.getString("name"));
+					if(p!=null)p.disconnect(Base.genBanReason(se, se.time == -1 ? "§8Indéfini" : TextUtils.secondsToTime(se.remaning())).create());
 
-				if(silent){
-					ChatComponent cc = new ChatComponent("§cSanction §ldiscrète§c : §8"+sender.getName()+"§c à banni §8"+se.on+"§c ! "+Main.c);
-					cc.setHoverEvent(se.getHover());
-					ServerUtils.permMsg("sanctions.notify.ban", cc.create());
+					if(silent){
+						ChatComponent cc = new ChatComponent("§cSanction §ldiscrète§c : §8"+sender.getName()+"§c à banni §8"+se.on+"§c ! "+Main.c);
+						cc.setHoverEvent(se.getHover());
+						ServerUtils.permMsg("sanctions.notify.ban", cc.create());
+					}else{
+						ChatComponent cc = new ChatComponent("§cSanction : §8"+sender.getName()+"§c à banni §8"+se.on+"§c ! "+Main.c);
+						cc.setHoverEvent(se.getHover());
+						Main.main.getProxy().broadcast(cc.create());
+					}
+					return "§c"+se.on +" à été banni avec succès !";
 				}else{
-					ChatComponent cc = new ChatComponent("§cSanction : §8"+sender.getName()+"§c à banni §8"+se.on+"§c ! "+Main.c);
-					cc.setHoverEvent(se.getHover());
-					Main.main.getProxy().broadcast(cc.create());
-				}
-				return "§c"+se.on +" à été banni avec succès !";
+					if (!se.by.equals(sender.getName())){
+						if (sender.hasPermission("sanctions.override.ban")) {
+							sender.sendMessage(ChatComponent.create("§4Attention : tu modifie une sanction qui n'est pas la tienne"));
+						}else{
+							return "§cTu ne peux pas modifier cette sanction car elle à été faite par "+se.by+" !";
+						}
+					}
 
+					if(args[1].equalsIgnoreCase("def")||args[1].equalsIgnoreCase("inf"))se.time = -1;
+					else {
+						se.time = TextUtils.timeToSeconds(args[1]);
+						if (se.time <= 0) return "§cTemps " + args[1] + " invalide !";
+					}
+
+					String reason = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
+					if(!reason.equals(""))se.reason = reason;
+
+					Main.sql.fastUpdate("UPDATE actuals SET time=?, reason=? WHERE id=?", se.time, se.reason, se.id);
+					Main.sql.fastUpdate("UPDATE history SET time=?, reason=? WHERE id=?", se.time, se.reason, se.id);
+
+					if(silent){
+						ChatComponent cc = new ChatComponent("§cSanction §ldiscrète§c : §8"+sender.getName()+
+								"§c à mis à jour le bannissement de §8"+se.on+"§c ! "+Main.c);
+						cc.setHoverEvent(se.getHover());
+						ServerUtils.permMsg("sanctions.notify.ban", cc.create());
+					}else{
+						ChatComponent cc = new ChatComponent("§cSanction : §8"+sender.getName()+
+								"§c à mis à jour le bannissement de  §8"+se.on+"§c ! "+Main.c);
+						cc.setHoverEvent(se.getHover());
+						Main.main.getProxy().broadcast(cc.create());
+					}
+					return "§cTu as bien modifié la sanction de §8"+se.on+"§c !";
+				}
 			}catch(SQLException e){
 				e.printStackTrace();
 				Main.sql.broadcastError();
--- src/fr/entasia/sanctions/commands/infos/HistoryCmd.java
+++ src/fr/entasia/sanctions/commands/infos/HistoryCmd.java
@@ -22,57 +22,52 @@ public class HistoryCmd extends Command {
 
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		if(sender.hasPermission("sanctions.use.history")){
-			sender.sendMessage(ChatComponent.create(execHist(sender, args)));
-		}else sender.sendMessage(ChatComponent.create("§cTu n'as pas accès à cette commande !"));
-	}
+		if(sender.hasPermission("sanctions.use.history")) {
+			if (args.length == 0) ChatComponent.create("§cSyntaxe : /history <pseudo> [options]"); // TODO options à faire
+			else {
+				try {
 
-	public static String execHist(CommandSender sender, String[] args){
-		if(args.length==0)return "§cSyntaxe : /history <pseudo> [options]"; // TODO options à faire
-		else{
-			try{
+					ResultSet rs = Main.sql.fastSelectUnsafe("SELECT * FROM history WHERE `on`=? ORDER BY `when` ASC", args[0]);
+					if (rs.next()) {
+						ChatComponent cc;
+						ChatComponent h;
+						SanctionEntry se;
+						String a;
+						sender.sendMessage(ChatComponent.create("§cSanctions de §8" + args[0] + "§c :"));
+						do {
+							se = new SanctionEntry(); // pas une vraie sanction, juste utilisée comme structure
+							se.id = rs.getInt("id");
+							se.on = rs.getString("on");
+							se.by = rs.getString("by");
+							se.when = Calendar.getInstance();
+							se.when.setTimeInMillis(rs.getLong("when"));
+							se.time = rs.getInt("time");
+							se.reason = rs.getString("reason");
 
-				ResultSet rs = Main.sql.fastSelectUnsafe("SELECT * FROM history WHERE `on`=? ORDER BY `when` ASC", args[0]);
-				if(rs.next()){
-					ChatComponent cc;
-					ChatComponent h;
-					SanctionEntry se;
-					String a;
-					sender.sendMessage(ChatComponent.create("§cSanctions de §8"+args[0]+"§c :"));
-					do{
-						se = new SanctionEntry(); // pas une vraie sanction, juste utilisée comme structure
-						se.id = rs.getInt("id");
-						se.on = rs.getString("on");
-						se.by = rs.getString("by");
-						se.when = Calendar.getInstance();
-						se.when.setTimeInMillis(rs.getLong("when"));
-						se.time = rs.getInt("time");
-						se.reason = rs.getString("reason");
+							cc = new ChatComponent("§c" + TextUtils.formatCalendar(se.when) + " §8" + (rs.getByte("type") == 0 ? "Ban" : "Mute"));
+							h = se.getInfos();
+							a = rs.getString("unban_by");
+							if (a != null) {
+								h.append("\n§cDébanni par : §8" + a);
+								se.when.setTimeInMillis(rs.getLong("unban_when"));
+								h.append("\n§cDébanni le : §8" + TextUtils.formatCalendar(se.when));
+								a = rs.getString("unban_reason");
+								h.append("\n§cDébanni pour raison : §8" + (a == null ? "§cIndéfinie" : a));
+							}
 
-						cc = new ChatComponent("§c"+TextUtils.formatCalendar(se.when)+" §8"+(rs.getByte("type") == 0 ? "Ban" : "Mute"));
-						h = se.getInfos();
-						a = rs.getString("unban_by");
-						if(a!=null){
-							h.append("\n§cDébanni par : §8"+a);
-							se.when.setTimeInMillis(rs.getLong("unban_when"));
-							h.append("\n§cDébanni le : §8"+TextUtils.formatCalendar(se.when));
-							a = rs.getString("unban_reason");
-							h.append("\n§cDébanni pour raison : §8"+(a ==null ? "§cIndéfinie" : a));
-						}
+							se.type = rs.getByte("type");
+							cc.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, h.create()));
 
-						se.type = rs.getByte("type");
-						cc.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, h.create()));
+							sender.sendMessage(cc.create());
 
-						sender.sendMessage(cc.create());
-
-					}while(rs.next());
-					return "§c------";
-				}else return "§cAucune sanction n'a été trouvée pour ce joueur ):";
-			}catch(SQLException e){
-				e.printStackTrace();
-				Main.sql.broadcastError();
-				return "§cUne erreur SQL s'est produite ! Contacte iTrooz_ !";
+						} while (rs.next());
+					} else sender.sendMessage(ChatComponent.create("§cAucune sanction n'a été trouvée pour ce joueur ):"));
+				} catch (SQLException e) {
+					e.printStackTrace();
+					Main.sql.broadcastError();
+					sender.sendMessage(ChatComponent.create("§cUne erreur SQL s'est produite ! Contacte iTrooz_ !"));
+				}
 			}
-		}
+		}else sender.sendMessage(ChatComponent.create("§cTu n'as pas accès à cette commande !"));
 	}
 }
